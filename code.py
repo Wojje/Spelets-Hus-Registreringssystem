5f9fc9c2e79350080001403b874f153452e158dd
Joppe Widstam <joppewidstam@hotmail.com>
2015-06-15 19:32:09 +0200

    version 1.0. lists checked in members and lists members that are still able to check in

--- code.py
+++ code.py
@@ -7,17 +7,22 @@ render = web.template.render('templates/')
 urls = ('/', 'index')
 app = web.application(urls, globals())
 
-myform = form.Form()
-
 class index: 
     def GET(self): 
-        members = db.select('member')
-        return render.index(members)
+        checkedIn = db.query("SELECT * FROM member WHERE member.member_id IN (\
+        SELECT member_id FROM registrations WHERE DATE(reg_time)=DATE(NOW()) AND \
+        member.member_id = registrations.member_id)")
+        
+        members = db.query("SELECT * FROM member WHERE member.member_id NOT IN (\
+        SELECT member_id FROM registrations WHERE DATE(reg_time)=DATE(NOW()) AND \
+        member.member_id = registrations.member_id)")
+        return render.index(checkedIn, members)
 
     def POST(self): 
         post_data=web.input(name=[])
         for checked in post_data.name:
             n = db.insert('registrations', member_id=checked)
+        raise web.seeother('/')
 
 if __name__=="__main__":
     web.internalerror = web.debugerror
