e73d6886e5000977c496161b2ca047732816f1b1
Pelle Reimers <per.reimers+github@gmail.com>
2015-06-16 16:22:23 +0200

    Add sqlalchemy database

--- server.py
+++ server.py
@@ -1,20 +1,61 @@
-from flask import Flask, render_template, redirect, url_for
-from collections import namedtuple
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import date
 
-Member = namedtuple("Member", "id name")
-pelle = Member(0, "Pelle")
-joppe = Member(1, "Joppe")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+db = SQLAlchemy(app)
+
+
+class Visitor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Visitor (%s)' % self.name
+
+     
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    visitor_id = db.Column(db.Integer, db.ForeignKey('visitor.id'))
+    visitor = db.relationship('Visitor',
+        backref=db.backref('visits', lazy='dynamic'))
+        
+    def __init__(self, visitor, date):
+        self.visitor = visitor
+        self.date = date
+          
+    def __repr__(self):
+        return 'Visit (%s, %s)' % (self.visitor.name, self.date)
+
 
 @app.route("/", methods=["GET"])
 def index():
-    checked_in = [pelle]
-    not_checked_in = [joppe]
+    not_checked_in = Visitor.query.all() # todo
+    checked_in = not_checked_in # todo
     return render_template("index.html", 
-                           checked_in=checked_in, not_checked_in=not_checked_in)
+                           checked_in=checked_in, not_checked_in=not_checked_in,
+                           visits=Visit.query.all())
 
 @app.route("/checked_in", methods=["POST"])
 def check_in():
+    visitor_id = request.form['visitor_id']
+    visitor = Visitor.query.get(visitor_id)
+    now = date.today()
+    new_visit = Visit(visitor, now)
+    db.session.add(new_visit)
+    db.session.commit()
+    return redirect(url_for('index'))
+    
+@app.route("/create_visitor", methods=["POST"])
+def create_visitor():
+    new_visitor = Visitor(request.form['name'])
+    db.session.add(new_visitor)
+    db.session.commit()
     return redirect(url_for('index'))
 
 
